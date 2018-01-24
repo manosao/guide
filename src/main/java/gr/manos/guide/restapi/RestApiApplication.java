1f9c28cc13b036c30e9b6d31c108e7ce9cabb6e8
Neurosoft\e.petrakis <e.petrakis@neurosoft>
2018-01-24 16:27:52 +0200

    Initial Spring Boot commit

---
+++ src/main/java/gr/manos/guide/restapi/RestApiApplication.java
@@ -0,0 +1,12 @@
+package gr.manos.guide.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiApplication.class, args);
+	}
+}
