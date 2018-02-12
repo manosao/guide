61171da5a0af8d181db4baadc5a9d881a733d3b1
Neurosoft\e.petrakis <e.petrakis@neurosoft>
2018-02-12 17:08:21 +0200

    class renamed

---
+++ src/main/java/gr/manos/guide/restapi/models/SpringSecurityUser.java
@@ -0,0 +1,104 @@
+package gr.manos.guide.restapi.models;
+
+import java.sql.Timestamp;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.Builder;
+import lombok.Data;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Data
+@Builder
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "users")
+public class SpringSecurityUser implements UserDetails {
+
+	private static final long serialVersionUID = 8841584421154392906L;
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	@NotNull
+	@Size(min=4, max=45)
+	private String username;
+	
+	@NotNull
+	@Size(min=4)
+//	@JsonIgnore
+	private String password;
+	
+	@CreatedDate
+	@Column(name="created_at")
+	private Date createdAt;
+	
+    @Column(name = "enabled")
+    private boolean enabled;
+
+    @Column(name = "last_password_reset_date")
+    private Timestamp lastPasswordResetDate;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(name = "users_authorities",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "authority_id", referencedColumnName = "id"))
+    private List<Authority> authorities;
+
+    public void setPassword(String password) {
+        Timestamp now = new Timestamp( new Date().getTime() );
+        this.setLastPasswordResetDate( now );
+        this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    
+}
